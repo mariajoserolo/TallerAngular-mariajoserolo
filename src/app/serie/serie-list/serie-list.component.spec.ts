77f569e9c4e1d45b33ee23d5ae3389dadcf0f9fb
Maria Jose Rodriguez <mj.rodriguezl1@uniandes.edu.co>
2025-10-26 23:59:44 -0500

    Hice lo que pude, no funcionó :(

--- src/app/serie/serie-list/serie-list.component.spec.ts
+++ src/app/serie/serie-list/serie-list.component.spec.ts
@@ -1,28 +1,42 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, inject } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { SerieService } from '../serie.service';
 import { DebugElement } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { SerieListComponent } from './serie-list.component';
+import { App } from '../../app';
+import { RouterTestingModule } from '@angular/router/testing';
 
-describe('SerieListComponent', () => {
-  let component: SerieListComponent;
-  let fixture: ComponentFixture<SerieListComponent>;
+describe('Service: Serie', () => {
+  //let component: SerieListComponent;
+  //let fixture: ComponentFixture<SerieListComponent>;
 
-  beforeEach(async(() => {
+  beforeEach((() => {
     TestBed.configureTestingModule({
-      declarations: [ SerieListComponent ]
-    })
+      declarations: [ App, SerieListComponent ],
+      imports: [ RouterTestingModule,HttpClientTestingModule],
+      providers: [SerieService]
+    });
     .compileComponents();
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SerieListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  //beforeEach(() => {
+    //fixture = TestBed.createComponent(SerieListComponent);
+    //component = fixture.componentInstance;
+    //fixture.detectChanges();
+  //});
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(App);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it(`should have as title 'TV Series'`, () => {
+    const fixture = TestBed.createComponent(App);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('TV Series');
+  })
 });
